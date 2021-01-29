edf1ed59de78bdbafe664cbc8acdb87ed0bdc4fc
CyanMY <47242851+CyanMY@users.noreply.github.com>
2021-01-29 11:59:33 +0800

    Update zhCN.lua

--- Localization/zhCN.lua
+++ Localization/zhCN.lua
@@ -12,18 +12,18 @@ L['DUNGEON'] = DUNGEONS
 L['CHARACTER'] = CHARACTER
 
 -- Subsection Headers
-L['CHARACTERS'] = 'CHARACTERS'
-L['AFFIXES'] = 'AFFIXES'
+L['CHARACTERS'] = '角色'
+L['AFFIXES'] = '词缀'
 
 -- Character Labels
-L['CURRENT_KEY'] = 'CURRENT'
-L['WEEKLY_BEST'] = 'WKLY BEST'
-
-L['CHARACTER_DUNGEON_NOT_RAN'] = 'No mythic+ ran'
-L['CHARACTER_KEY_NOT_FOUND'] = 'No key found'
+L['CURRENT_KEY'] = '当前钥石'
+L['WEEKLY_BEST'] = '本周最佳'
+L['CHARACTER_DUNGEON_NOT_RAN'] = '未进行过史诗钥石地下城'
+L['CHARACTER_KEY_NOT_FOUND'] = '未发现钥石'
 
 
 -- Dropdown menu selections
+L['REPORT_TO'] = '将钥石通报至'
 L['Whisper'] = WHISPER
 L['INVITE'] = INVITE
 L['SUGGEST_INVITE'] = SUGGEST_INVITE
@@ -31,15 +31,28 @@ L['REQUEST_INVITE'] = REQUEST_INVITE
 L['CANCEL'] = CANCEL
 
 -- Announce messages
-L['ANNOUNCE_NEW_KEY'] = 'n Keys: New key %s'
-L['NO_KEY'] = 'No key'
-L['KEYS_RESPOND_ON_NO_KEY'] = 'Respond even if you do not have a key'
+L['ANNOUNCE_NEW_KEY'] = 'n Keys: 新钥石 %s'
+L['NO_KEY'] = '没有钥石'
+L['KEYS_RESPOND_ON_NO_KEY'] = '没有钥石'
 
 
 -- Search field texts
-L['FILTER_TEXT_DUNGEON'] = 'Filter by dungeon'
-L['FILTER_TEXT_CHARACTER'] = 'Filter by character name'
+L['FILTER_TEXT_DUNGEON'] = '按地下城过滤'
+L['FILTER_TEXT_CHARACTER'] = '按角色过滤'
 
 -- Options
-L['!KEYS_DESC'] = 'Respond to !keys command in the following chat channels'
-L['EXPANDED_TOOLTIP'] = 'Show affix description in the tooltip'
+L['GENERAL OPTIONS'] = '一般设置'
+L['Show offline players'] = '显示在线玩家'
+L['Show Minimap button'] = '显示小地图按钮'
+L['Show current key in tooltip'] = '鼠标提示中显示当前钥石'
+L['Display offline below online'] = '在线玩家在离线玩家上方'
+L['Announce new keys to party'] = '向小队通报新钥石'
+L['Announce new keys to guild'] = '向公会通报新钥石'
+L['EXPANDED_TOOLTIP'] = '鼠标提示中显示词缀详细说明'
+L['!keys chat command'] = '!keys 命令'
+L['!KEYS_DESC'] = '下列聊天频道收到 !keys 时进行回应'
+L['SYNC OPTIONS'] = '同步设置'
+L['Sync with friends'] = '同步好友'
+L['Show other faction'] = '显示其他阵营'
+L['Rank Filter'] = '名单过滤'
+L['Include these ranks in the guild listing'] = '公会名单中包含以下会阶'
